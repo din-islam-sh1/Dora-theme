11c8beb3f821e16df4fec6ba5aef8df9b5a75aad
FazlyRabbi <rabbisam74@gmail.com>
2023-06-10 10:50:00 +0600

    add js folder

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+const print = document.querySelector(".print");
+
+print.innerText = "hello";
+
+
+
+
+// assignment
+
+//   declare several variables with different data types. The name variable holds a string, age holds a number, isStudent holds a boolean value, 
+
+// The print statements are used to output the values of these variables to the web page. 
