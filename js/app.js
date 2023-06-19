136a3ed9c3ed4d0d1c150cde17f1c75f6b92d86e
FazlyRabbi <rabbisam74@gmail.com>
2023-06-19 10:40:05 +0600

    add 4 assignmnt

--- js/app.js
+++ js/app.js
@@ -1,13 +1,63 @@
 
 const print = document.querySelector(".print");
 
-print.innerText = "hello";
+// const name = null ;
+
+// const age = 18;
+
+// const  color = "red"
+
+// let bike ;
+
+// const isStudent = true;
+
+//  console.log(typeof name);
+
+//  console.log(typeof age);
+
+//  console.log(typeof color);
+
+//  console.log(typeof isStudent);
+
+//  console.log(typeof bike);
+
+// print.innerText = `${ typeof age}`;
+
+// const obj = {
+//     name: "rabbi",
+//     age: 14,
+//     isStudent: false
+// }
 
 
 
+// =============================================
 
-// assignment
+// Assignment: Variables and Data Types
+// Task 1: Declare variables
+// Declare three variables: name, age, and isStudent.
+// Assign your own values to each variable.
+// Make sure to use appropriate data types.
 
-//   declare several variables with different data types. The name variable holds a string, age holds a number, isStudent holds a boolean value, 
+// TODO: Declare variables
 
-// The print statements are used to output the values of these variables to the web page. 
+// Task 2: Display variable values
+// Print the values of the variables to the console.
+// Make sure to use console.log().
+
+// TODO: Display variable values
+
+
+// Task 3: Data type conversion
+// Convert the age variable from number to string data type.
+// Store the converted value in a new variable called ageAsString.
+
+// TODO: Data type conversion
+
+
+// Task 4: Concatenate and display
+// Create a new variable called personalInfo that stores the concatenation
+// of the name and ageAsString variables.
+// Print the value of the personalInfo variable to the console.
+
+// TODO: Concatenate and display
